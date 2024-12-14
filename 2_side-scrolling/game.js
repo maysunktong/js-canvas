ae639bfed0d33e63615390f2c1b5473bac91a1ad
May Pattaranan <56375291+maysunktong@users.noreply.github.com>
2024-12-14 23:44:24 +0100

    Add key bindings

--- 2_side-scrolling/game.js
+++ 2_side-scrolling/game.js
@@ -3,6 +3,8 @@ const ctx = canvas.getContext("2d");
 
 canvas.width = innerWidth;
 canvas.height = innerHeight;
+
+const gravity = 0.5;
 class Player {
   constructor() {
     this.position = {
@@ -12,9 +14,9 @@ class Player {
     this.width = 30;
     this.height = 30;
     this.velocity = {
-      // player will draw downward only
+      // player will draw downward only, y-axis
       x: 0,
-      y: 1,
+      y: 0,
     };
   }
 
@@ -24,18 +26,79 @@ class Player {
   }
 
   update() {
-    this.position.y += this.velocity.y;
     this.draw();
+    this.position.y += this.velocity.y;
+    this.position.x += this.velocity.x;
+
+    if (this.position.y + this.height + this.velocity.y <= canvas.height)
+      this.velocity.y += gravity;
+    else this.velocity.y = 0;
   }
 }
 
 const player = new Player();
-player.update();
-
-// animate()
-const animate = () => {
-  requestAnimationFrame(animate);
-  player.update();
+const keys = {
+  right: {
+    pressed: false,
+  },
+  left: {
+    pressed: false,
+  },
 };
 
+// loop over animate()
+const animate = () => {
+  requestAnimationFrame(animate);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  player.update();
+
+  // keys management
+  if (keys.right.pressed) {
+    player.velocity.x = 5;
+  } else if (keys.left.pressed) {
+    player.velocity.x = -5;
+  } else {
+    player.velocity.x = 0;
+  }
+};
 animate();
+
+addEventListener("keydown", ({ keyCode }) => {
+  switch (keyCode) {
+    case 87:
+      console.log("up");
+      player.velocity.y -= 20;
+      break;
+    case 83:
+      console.log("down");
+      break;
+    case 65:
+      console.log("left");
+      keys.left.pressed = true;
+      break;
+    case 68:
+      console.log("right");
+      keys.right.pressed = true;
+      break;
+  }
+});
+
+addEventListener("keyup", ({ keyCode }) => {
+  switch (keyCode) {
+    case 87:
+      console.log("up");
+      player.velocity.y = 0;
+      break;
+    case 83:
+      console.log("down");
+      break;
+    case 65:
+      console.log("left");
+      keys.left.pressed = false;
+      break;
+    case 68:
+      console.log("right");
+      keys.right.pressed = false;
+      break;
+  }
+});
