50fb244db4d5a3013ed2158e8eda2b30f14fd239
May Pattaranan <56375291+maysunktong@users.noreply.github.com>
2024-12-14 01:04:09 +0100

    Implement randomed spawned enemies

--- 1_base-defense/script.js
+++ 1_base-defense/script.js
@@ -43,6 +43,27 @@ class Projectile {
   }
 }
 
+class Enemy {
+  constructor(x, y, radius, color, velocity) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = velocity;
+  }
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+  }
+  update() {
+    this.draw();
+    this.x = this.x + this.velocity.x;
+    this.y = this.y + this.velocity.y;
+  }
+}
+
 // Player positions in the middle of screen
 const x = canvas.width / 2;
 const y = canvas.height / 2;
@@ -50,6 +71,40 @@ const y = canvas.height / 2;
 const player = new Player(x, y, 30, "blue");
 
 const projectiles = [];
+const enemies = [];
+
+function spawnEnemies() {
+  setInterval(() => {
+    // Make sure enemies are not too small
+    const radius = Math.random() * (30 - 10) + 10;
+
+    let x;
+    let y;
+
+    if (Math.random() < 0.5) {
+      x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius;
+      y = Math.random() * canvas.height;
+    } else {
+      x = Math.random() * canvas.height;
+      y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius;
+    }
+
+    const color = "green";
+
+    // Object move towards center
+    const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x);
+
+    // Object away from center
+    // const angle = Math.atan2(y-canvas.height / 2, x-canvas.width / 2);
+
+    const velocity = {
+      x: Math.cos(angle),
+      y: Math.sin(angle),
+    };
+
+    enemies.push(new Enemy(x, y, radius, color, velocity));
+  }, 1000);
+}
 
 // loop over animate()
 function animate() {
@@ -59,6 +114,14 @@ function animate() {
   player.draw();
   projectiles.forEach((projectile) => {
     projectile.update();
+  });
+  enemies.forEach((enemy) => {
+    enemy.update();
+
+    projectiles.foreEach(projectile =>{
+      // distance btw two points
+      const distance = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)
+    })
   });
 }
 
@@ -79,3 +142,4 @@ window.addEventListener("click", (event) => {
 });
 
 animate();
+spawnEnemies();
